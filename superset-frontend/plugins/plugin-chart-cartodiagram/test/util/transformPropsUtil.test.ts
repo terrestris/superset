8f7e1cf688086e1991a6d75babaeb3034c561c36
Jakob Miksch <jakob@meggsimum.de>
2023-01-12 11:16:44 +0100

    Add test for parseSelectedChart()

--- superset-frontend/plugins/plugin-chart-cartodiagram/test/util/transformPropsUtil.test.ts
+++ superset-frontend/plugins/plugin-chart-cartodiagram/test/util/transformPropsUtil.test.ts
@@ -43,7 +43,19 @@ describe('getEchartConfigs', () => {
 });
 
 describe('parseSelectedChart', () => {
-  it('exists', () => {
-    expect(parseSelectedChart).toBeDefined();
+  it('parses the inline stringified JSON', () => {
+    const selectedChartObject = {
+      id: 278,
+      params:
+        '{"adhoc_filters":[],"applied_time_extras":{},"datasource":"24__table","viz_type":"pie","time_range":"No filter","groupby":["nuclide"],"metric":{"expressionType":"SIMPLE","column":{"advanced_data_type":null,"certification_details":null,"certified_by":null,"column_name":"nuclide","description":null,"expression":null,"filterable":true,"groupby":true,"id":772,"is_certified":false,"is_dttm":false,"python_date_format":null,"type":"TEXT","type_generic":1,"verbose_name":null,"warning_markdown":null},"aggregate":"COUNT","sqlExpression":null,"isNew":false,"datasourceWarning":false,"hasCustomLabel":false,"label":"COUNT(nuclide)","optionName":"metric_k6d9mt9zujc_7v9szd1i0pl"},"dashboards":[]}',
+      slice_name: 'pie',
+      viz_type: 'pie',
+    };
+
+    const selectedChartString = JSON.stringify(selectedChartObject);
+    const result = parseSelectedChart(selectedChartString);
+    const expectedParams = JSON.parse(selectedChartObject.params);
+
+    expect(result.params).toEqual(expectedParams);
   });
 });
