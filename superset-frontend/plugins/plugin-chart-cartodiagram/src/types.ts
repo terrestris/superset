e8838fa6ab97680ed67e0b11c39425e1dd120c51
Jan Suleiman <jansule@users.noreply.github.com>
2025-06-17 15:32:19 +0200

    feat(ter): add support for WKB and WKT on cartodiagram plugin (#19)

--- superset-frontend/plugins/plugin-chart-cartodiagram/src/types.ts
+++ superset-frontend/plugins/plugin-chart-cartodiagram/src/types.ts
@@ -25,6 +25,7 @@ import { Coordinate } from 'ol/coordinate';
 import { Map } from 'ol';
 import { Feature, FeatureCollection, Point } from 'geojson';
 import { Style } from 'geostyler-style';
+import { GeometryFormat } from './constants';
 
 export interface CartodiagramPluginStylesProps {
   height: number;
@@ -43,6 +44,7 @@ export type ChartConfig = FeatureCollection<
 
 interface CartodiagramPluginCustomizeProps {
   geomColumn: string;
+  geomFormat: GeometryFormat;
   selectedChart: string;
   chartConfigs: ChartConfig;
   chartSize: ZoomConfigs;
