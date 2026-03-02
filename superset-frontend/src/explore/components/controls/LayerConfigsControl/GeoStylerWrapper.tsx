5c5d025dbd253ede9d61be6e3d1406563c3f3ac5
Michael Holthausen <mholthausen@terrestris.de>
2026-03-02 15:54:16 +0100

    fix(ter): fixes cartodiagram

--- superset-frontend/src/explore/components/controls/LayerConfigsControl/GeoStylerWrapper.tsx
+++ superset-frontend/src/explore/components/controls/LayerConfigsControl/GeoStylerWrapper.tsx
@@ -24,7 +24,6 @@
  */
 import { CardStyle } from 'geostyler/dist/Component/CardStyle/CardStyle';
 import { FC } from 'react';
-import { ConfigProvider } from 'antd-v5';
 import { GeoStylerWrapperProps } from './types';
 import 'geostyler/dist/index.css';
 
